6a76e246b63b6e2ef750bd0433da6a1e96ae7591
hwna00 <layton3534@gmail.com>
2023-11-24 16:23:37 +0900

    <Test>: 테스트코드 수정
    - 파일이름 수정: clientRegister -> registerClient
    - 각 테스트에서 공통적으로 수행되는 로직 분리
    - MemoryRouter를 사용하여 테스트 환경에 라우팅 컨텍스트 제공

--- tcpClient/client-gui/src/__tests__/registerClient.test.js
+++ tcpClient/client-gui/src/__tests__/registerClient.test.js
@@ -1,45 +1,58 @@
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-
+import { MemoryRouter } from "react-router-dom";
 import RegisterPage from "../views/RegisterPage";
 import { useSocket } from "../hooks";
 
 jest.mock("../hooks");
 
 describe("Register Page", () => {
-  window.alert = jest.fn();
-  window.scrollTo = jest.fn();
+  const mockEmit = jest.fn();
+  const mockOn = jest.fn();
+
+  beforeEach(() => {
+    window.alert = jest.fn();
+    window.scrollTo = jest.fn();
+    useSocket.mockReturnValue({ emit: mockEmit, on: mockOn });
+  });
+
+  afterEach(() => {
+    window.alert.mockRestore();
+    window.scrollTo.mockRestore();
+    jest.clearAllMocks();
+  });
 
   it("도메인 네임 변환 버튼 클릭 시, ip 주소 값과 hex 주소 값을 가져와야 한다", async () => {
-    const mockEmit = jest.fn();
-    const mockOn = jest.fn((event, callback) => {
+    mockOn.mockImplementation((event, callback) => {
       if (event === "domain_to_address") {
         callback({ ip: "192.168.1.1", hex: "C0A80101" });
       }
     });
-    useSocket.mockReturnValue({ emit: mockEmit, on: mockOn });
 
-    render(<RegisterPage />);
+    render(
+      <MemoryRouter>
+        <RegisterPage />
+      </MemoryRouter>
+    );
+
     fireEvent.change(
       screen.getByPlaceholderText(
-        "연결하고자 하는 서버의 도메인 주소를 입력하세요",
-        { target: { value: "test value" } }
-      )
+        "연결하고자 하는 서버의 도메인 주소를 입력하세요"
+      ),
+      { target: { value: "test value" } }
     );
     fireEvent.click(screen.getByText("변환하기"));
 
     await waitFor(() => {
       expect(screen.getByDisplayValue("192.168.1.1")).toBeInTheDocument();
     });
-
     await waitFor(() => {
       expect(screen.getByDisplayValue("C0A80101")).toBeInTheDocument();
     });
   });
 
   it("연결하기 버튼 클릭 시, 3-way handshake 과정을 확인할 수 있어야 한다.", async () => {
-    const mockEmit = jest.fn();
-    const mockOn = jest.fn((event, callback) => {
+    mockOn.mockImplementation((event, callback) => {
       switch (event) {
         case "handshake_request":
           callback({ msg: "handshake_request" });
@@ -54,10 +67,23 @@ describe("Register Page", () => {
           break;
       }
     });
-    useSocket.mockReturnValue({ emit: mockEmit, on: mockOn });
 
-    render(<RegisterPage />);
+    render(
+      <MemoryRouter>
+        <RegisterPage />
+      </MemoryRouter>
+    );
 
+    fireEvent.change(
+      screen.getByPlaceholderText(
+        "연결하고자 하는 서버의 도메인 주소를 입력하세요"
+      ),
+      { target: { value: "test value" } }
+    );
+    fireEvent.change(
+      screen.getByPlaceholderText("채팅에서 사용할 닉네임을 입력해주세요"),
+      { target: { value: "nickname" } }
+    );
     fireEvent.click(screen.getByText("연결하기"));
 
     await waitFor(() => {
@@ -70,7 +96,4 @@ describe("Register Page", () => {
       ).toBeInTheDocument();
     });
   });
-
-  window.alert.mockRestore();
-  window.scrollTo.mockRestore();
 });
