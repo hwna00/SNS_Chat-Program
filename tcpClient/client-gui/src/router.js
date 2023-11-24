e93a418e6711e7031c9a4198eb14c10b7a0cf4f3
hwna00 <layton3534@gmail.com>
2023-11-24 20:41:38 +0900

    <Feat>: 메인 페이지 유닛 테스트 작성
    - 웹소켓 통신으로 받아온 채팅 목록이 랜더링되는 지 테스트
    - 새로운 채팅목록이 들어왔을 때, 채팅방 목록이 업데이트 되는 지 테스트
    
    ref: #2

--- tcpClient/client-gui/src/router.js
+++ tcpClient/client-gui/src/router.js
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import DefaultLayout from "./layout/DefaultLayout";
 import RegisterPage from "./views/RegisterPage";
+import MainPage from "./views/MainPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <DefaultLayout />,
     children: [
+      {
+        path: "",
+        element: <MainPage />,
+      },
       {
         path: "enter-page",
         element: <RegisterPage />,
