d490dcacd1acba89ae737d71b53b1304c43e5530
hwna00 <layton3534@gmail.com>
2023-11-27 00:42:53 +0900

    <Feat>: 채팅방 UI 작성
    - 우측 화면에서 메시지 입력 및 메시지 변환 결과 확인 가능
    - 좌측 화면에서 채팅 내역 확인 가능

--- tcpClient/client-gui/src/views/ChatRoom.js
+++ tcpClient/client-gui/src/views/ChatRoom.js
@@ -1,5 +1,62 @@
+import {
+  Avatar,
+  Button,
+  HStack,
+  Input,
+  Text,
+  UnorderedList,
+  VStack,
+} from "@chakra-ui/react";
+
+const ChatBubble = () => {
+  return (
+    <VStack justifyContent="center" alignItems="flex-start" gap="1">
+      <Avatar boxSize="6" />
+      <Text
+        width="fitContent"
+        px="4"
+        py="1"
+        marginLeft="6"
+        borderRadius="md"
+        borderTopLeftRadius="none"
+        bgColor="blue.100"
+      >
+        hihihihihihihihihihi
+      </Text>
+    </VStack>
+  );
+};
+
 const ChatRoom = () => {
-  return "chat room";
+  return (
+    <HStack height="full" alignItems="flex-start">
+      <UnorderedList
+        flex={1}
+        height="full"
+        margin="0"
+        padding="4"
+        listStyleType="none"
+        spacing="4"
+        borderRadius="sm"
+        bgColor="gray.100"
+        overflowY="scroll"
+      >
+        <ChatBubble />
+        <ChatBubble />
+        <ChatBubble />
+      </UnorderedList>
+
+      <VStack flex={1} alignItems="flex-start">
+        <HStack width="full" mb="8">
+          <Input />
+          <Button>전송</Button>
+        </HStack>
+
+        <Input readOnly placeholder="변환 결과" />
+        <Input readOnly placeholder="정렬 결과" />
+      </VStack>
+    </HStack>
+  );
 };
 
 export default ChatRoom;
