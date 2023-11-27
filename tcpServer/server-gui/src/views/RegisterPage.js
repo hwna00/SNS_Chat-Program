358dbc253d97f741b15097b819e4c9eedf141dce
hwna00 <layton3534@gmail.com>
2023-11-27 18:59:15 +0900

    <Feat>: 서버 등록 페이지 제작
    - 생성할 서버의 도메인 주소 입력 화면 구현
    - 입력한 서버의 도메인 주소를 변환한 결과를 확인할 수 있는 화면 구현
    - 생성한 서버에 최대로 연결을 허용할 소켓을 지정하는 화면 구현
    - 소켓 이벤트 연결
    
    ref: #4, #8 ,#9

--- tcpServer/server-gui/src/views/RegisterPage.js
+++ tcpServer/server-gui/src/views/RegisterPage.js
@@ -1,5 +1,124 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  Collapse,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useSocket } from "../hooks";
+
 const RegisterPage = () => {
-  return "register";
+  const socket = useSocket();
+
+  const { isOpen, onOpen } = useDisclosure();
+  const navigate = useNavigate();
+
+  const [domainName, setDomainName] = useState("");
+  const [maxConn, setMaxConn] = useState(8);
+  const [byte, setBtye] = useState("");
+  const [orderedByte, setOrderedByte] = useState("");
+  const [isConverting, setIsConverting] = useState(false);
+  const [isConnecting, setIsConnecting] = useState(false);
+
+  const convertDomainNameToAddress = () => {
+    if (domainName === "") {
+      return window.alert("도메인 주소를 입력해주세요");
+    }
+
+    setIsConverting(true);
+    socket.emit("domain_to_address");
+  };
+
+  const handleDomainNameChange = (event) => {
+    setDomainName(event.target.value);
+  };
+
+  const handleMaxConnChange = (event) => {
+    setMaxConn(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setIsConnecting(true);
+    socket.emit("create_server", { domainName, maxConn });
+  };
+
+  useEffect(() => {
+    if (!socket) {
+      return;
+    }
+
+    socket.on("domain_to_address", (payload) => {
+      setIsConverting(false);
+      onOpen();
+      setBtye(payload.byte);
+      setOrderedByte(payload.orderedByte);
+    });
+
+    socket.on("create_server", () => {
+      setIsConnecting(false);
+      window.localStorage.setItem("domainName", domainName);
+      navigate("/");
+    });
+
+    socket.on("error", (payload) => {
+      setIsConnecting(false);
+      window.alert(payload);
+    });
+  }, [socket, onOpen, navigate, domainName]);
+
+  return (
+    <VStack as="form" onSubmit={handleSubmit}>
+      <FormControl>
+        <FormLabel>개설할 서버의 도메인 주소를 입력하세요</FormLabel>
+        <HStack>
+          <Input
+            required
+            value={domainName}
+            onChange={handleDomainNameChange}
+          />
+          <Button
+            colorScheme="blue"
+            onClick={convertDomainNameToAddress}
+            isLoading={isConverting}
+          >
+            변환하기
+          </Button>
+        </HStack>
+      </FormControl>
+
+      <Collapse style={{ width: "100%" }} in={isOpen} animateOpacity>
+        <FormControl>
+          <FormLabel>변환된 IP 주소</FormLabel>
+          <Input readOnly value={byte} />
+        </FormControl>
+        <FormControl>
+          <FormLabel>변환된 bin 주소</FormLabel>
+          <Input readOnly value={orderedByte} />
+        </FormControl>
+      </Collapse>
+
+      <FormControl>
+        <FormLabel>연결 가능한 소켓 개수</FormLabel>
+        <Input type="number" value={maxConn} onChange={handleMaxConnChange} />
+      </FormControl>
+
+      <Button
+        type="submit"
+        width="full"
+        colorScheme="blue"
+        mt="8"
+        isLoading={isConnecting}
+      >
+        서버 생성
+      </Button>
+    </VStack>
+  );
 };
 
 export default RegisterPage;
