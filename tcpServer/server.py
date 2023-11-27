3de93e194f0fc9148bb8ea7f126f36cbf057061b
ghdtpdnjs <hsw1805@kookmin.ac.kr>
2023-11-27 12:11:43 +0900

    <Feat>:서버에서 클라이언트로 전송하는 웹소켓 이벤트 처리
    사용자가 입력한 도메인네임을 ip 주소로 변경
    변경된 ip주소를 binary로 변환
    문자열 메세지를 변환한 결과를 이벤트를 통해 클라이언트에게 전달
    TCP/IP 서버 소켓으로부터 넘겨 받은 채팅 메세지를 클라이언트에게 전달

--- tcpServer/server.py
+++ tcpServer/server.py
@@ -1,7 +1,55 @@
 import socket
 import threading
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
 
 client_sockets = []
+
+
+
+@socketio.on('domain_to_address')
+def handle_domain_to_address(data):
+    domainName=data['domainName']
+    #도메인 네임 -> ip주소 -> binary변환
+    ipAddress = socket.gethostbyname(domainName)
+    binaryAddress= socket.inet_pton(socket.AF_INET, ipAddress)
+    
+    socketio.emit('domain_to_address',{'domainName':binaryAddress})
+
+def handshake_request():
+    return
+
+def handshake_response():
+    return
+
+#tcp/ip소켓 생성 결과 확인
+def make_connection(client_info):
+    #이건 나중에 수정
+        return
+
+#byte little endian으로 변환 후 hex로 변환
+def convert_msg_to_byte(msg):
+    byteMsg = bytes(msg, 'utf-8')
+    orderedByteMsg = bytearray(reversed( byteMsg))
+    return {'byteMsg': byteMsg.hex(),'orderByteMsg':orderedByteMsg.hex()}
+
+
+#클라이언트로 전송
+@socketio.on('msg_to_byte')
+def handle_msg_to_byte(msg):
+    msg = convert_msg_to_byte(msg)
+    socketio.emit('msg_to_byte', msg)
+
+@socketio.on('recv_msg')
+def handle_recv_msg(data):
+    sender = data['sender']
+    room_name = data['roomName']
+    message = data['msg']
+    #전송 데이터 예시: { 'sender': '홍길동', 'roomName': '홍철범', msg: '반갑수당' }
+    socketio.emit('recv_msg', {'sender': sender, 'roomName': room_name, 'msg': message})
 
 def client_thread(client_socket, addr):
     try:
@@ -10,12 +58,15 @@ def client_thread(client_socket, addr):
             if not data: break
             print("메세지를 전송받았습니다 : ", addr)
             print("메세지 내용 : ", data.decode())
+
             client_socket.send(data)
         client_socket.close()
     except ConnectionResetError as e:
         print("Disconnected by User : ", addr)
     client_sockets.remove(client_socket)
     client_socket.close()
+
+
 
 if __name__ == "__main__":
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -34,3 +85,4 @@ if __name__ == "__main__":
             print("현재 연결된 사용자 수 : ", len(client_sockets))
             thread.start()
     server_socket.close()
+    socketio.run(app)
