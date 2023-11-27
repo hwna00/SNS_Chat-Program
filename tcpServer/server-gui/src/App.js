d6c6cbe9a6c3971d8c9e960200af17b67706faa7
hwna00 <layton3534@gmail.com>
2023-11-27 13:11:00 +0900

    <Feat>: 서버 GUI 레이아웃 작성
    - 클라이언트 GUI를 참고하여 기본 레이아웃 작성
    
    ref: #4

--- tcpServer/server-gui/src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return "server";
-}
-
-export default App;
---
+++ tcpServer/server-gui/src/layouts/DefaultLayout.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+
+import { Outlet } from "react-router-dom";
+import { Container, HStack, Heading, VStack } from "@chakra-ui/react";
+import { io } from "socket.io-client";
+
+import SocketContext from "../components/SocketContext";
+
+const DefaultLayout = () => {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const webSocket = io("");
+    setSocket(webSocket);
+
+    return () => webSocket.close();
+  }, []);
+
+  return (
+    <VStack width="100vw" height="100vh">
+      <HStack width="full" padding="4" boxShadow="base">
+        <Heading
+          as="h1"
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          bgClip="text"
+        >
+          SNS TALK - Server
+        </Heading>
+      </HStack>
+      <Container mt="8">
+        <SocketContext.Provider value={socket}>
+          <Outlet />
+        </SocketContext.Provider>
+      </Container>
+    </VStack>
+  );
+};
+
+export default DefaultLayout;
