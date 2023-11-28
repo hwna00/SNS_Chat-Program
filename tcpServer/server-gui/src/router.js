ec0541fdc131a9199efb928d92020893ec33882c
hwna00 <layton3534@gmail.com>
2023-11-28 12:15:23 +0900

    <Test>: 서버 메인 페이지 테스트 코드 작성
    - 현재 실행중인 서버 주소를 확인할 수 있는가
    - 현재 서버에 연결된 소켓의 개수를 확인할 수 있는가
    - 현재 서버에 연결된 소켓 목록을 확인할 수 있는가
    - 전체 데이터 전송 내역을 확인할 수 있는가
    - 특정 소켓의 데이터 전송 내역을 확인할 수 있는가
    
    ref: #5

--- tcpServer/server-gui/src/router.js
+++ tcpServer/server-gui/src/router.js
@@ -1,12 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+
 import DefaultLayout from "./layouts/DefaultLayout";
 import RegisterPage from "./views/RegisterPage";
+import MainPage from "./views/MainPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <DefaultLayout />,
     children: [
+      {
+        path: "",
+        element: <MainPage />,
+      },
       {
         path: "register",
         element: <RegisterPage />,
