26dd7264bdc911a6e7457d2d540d3cb4d463e531
always_spring01 <hangbum01@gmail.com>
2023-11-30 22:59:50 +0900

    <Refactor> : TCP/IP 코드 리팩토링
    - TCP/IP 서버, 클라이언트 코드 리팩토링
    
    ref #32

---
+++ tcpClient/client_tcp.py
@@ -0,0 +1,71 @@
+from socket import *
+from threading import *
+
+class client_tcp():
+    def __init__(self, ip="127.0.0.1", port="9000", timeout=2000):
+        self.ip = ip
+        self.port = int(port)
+        self.timeout = timeout
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.settimeout(timeout)
+        self.stop_flag = False
+
+    def connect(self):
+        try:
+            self.socket.connect((self.ip, self.port))
+            print(f"{self.ip}:{self.port} : Connected to Server")
+        except TimeoutError as e:
+            print(f"{self.ip}:{self.port} : Connection Fail (Timeout Error)")
+
+    def recv(self):
+        try:
+            while not self.stop_flag:
+                if self.socket.fileno() == -1:
+                    print(f"{self.ip}:{self.port} : Disconnected by Server"); break
+                data = self.socket.recv(65535)
+                if not data:
+                    print(f"{self.ip}:{self.port} : Disconnected from Server"); break
+                if data.decode() == "SYN-ACK":
+                    self.socket.sendall(data)
+                elif data.decode() == "SERVER-CLOSE":
+                    self.socket.close()
+                else:
+                    print(f"{self.ip}:{self.port} : {data.decode()}")
+        except ConnectionAbortedError as e:
+            print(f"{self.ip}:{self.port} : Disconnected from Server")
+        except Exception as e2:
+            print(f"{self.ip}:{self.port} : Error occured in recv - {e2}")
+        finally:
+            self.socket.close()
+            return
+    
+    def send(self, data):
+        try:
+            self.socket.sendall(data.encode())
+            print(f"{self.ip}:{self.port} : Send Success")
+        except ConnectionRefusedError as e:
+            print(f"{self.ip}:{self.port} : Server closed")
+            self.socket.close()
+        except Exception as e:
+            print(f"{self.ip}:{self.port} : Send Failed - {e}")
+    
+    def start_client_tcp(self):
+        self.connect()
+        self.thread = Thread(target=self.recv, args=())
+        self.thread.start()
+
+    def stop_client_tcp(self):
+        self.stop_flag = True
+        self.socket.close()
+        self.thread.join()
+                    
+if __name__ == "__main__":
+    client = client_tcp()
+    client.start_client_tcp()
+
+    while True:
+        n = input()
+        if n == "quit":
+            client.stop_client_tcp()
+            break
+        client.send(n)
---
+++ tcpServer/server_tcp.py
@@ -0,0 +1,77 @@
+from socket import *
+from threading import *
+
+class server_tcp():
+    def __init__(self, ip="127.0.0.1", port="9000", max_connection=5):
+        self.ip = ip
+        self.port = int(port)
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.max_connection = max_connection
+        self.stop_flag = False
+        self.client_list = []
+    
+    def stop_server_tcp(self):
+        self.socket.close()
+        for clients in self.client_list:
+            clients.send(b"SERVER-CLOSE")
+        self.stop_flag = True
+    
+    def handshaking(self, client_socket, client_addr):
+        client_socket.send(b"SYN-ACK")
+        data = client_socket.recv(1024)
+        if data.decode() == "SYN-ACK":
+            print(f"{client_addr[0]}:{client_addr[1]} : Handshaked success")
+            return True
+        print(f"{client_addr[0]}:{client_addr[1]} : Handshake fail")
+        return False
+
+    def client_thread(self, client_socket, client_addr):
+        try:
+            while True:
+                data = client_socket.recv(65535)
+                if not data: break
+                print(f"{client_addr[0]}:{client_addr[1]} : {data.decode()}")
+                for client in self.client_list:
+                    client.sendall(data)
+        except ConnectionResetError as e:
+            print(f"{client_addr[0]}:{client_addr[1]} : Disconnected by User")
+        except ConnectionAbortedError as e:
+            print(f"{client_addr[0]}:{client_addr[1]} : Disconnected by User")
+        if client_socket in self.client_list:
+            self.client_list.remove(client_socket)
+        print(f"{client_addr[0]}:{client_addr[1]} : Disconnected by User")
+        client_socket.close()
+
+    def tcp_server(self):
+        try:
+            self.socket.bind((self.ip, self.port))
+            self.socket.listen()
+            print(f"Server Started at {self.ip}:{self.port}")
+            while not self.stop_flag:
+                client_socket, client_addr = self.socket.accept()
+                if len(self.client_list) >= self.max_connection:
+                    print(f"Server connection Fail : MAX CONNECTION ERROR")
+                    client_socket.close(); continue
+                if self.handshaking(client_socket, client_addr) is False:
+                    print(f"Server connection Fail : HANDSHAKING ERROR")
+                    client_socket.close(); continue
+                self.client_list.append(client_socket)
+                client = Thread(target=self.client_thread, args=(client_socket, client_addr))
+                print(f"{client_addr[0]}:{client_addr[1]} : Connected {len(self.client_list)}/{self.max_connection}")
+                client.start()
+            self.socket.close()
+        except OSError as e:
+            print(f"Server Closed by User")
+    
+    def start_tcp_server(self):
+        server_thread = Thread(target=self.tcp_server, args=())
+        server_thread.start()
+
+if __name__ == "__main__":
+    server = server_tcp()
+    server_thread = Thread(target=server.start_tcp_server, args=())
+    server_thread.start()
+
+    while True:
+        n = input()
+        if n == "quit": server.stop_server_tcp(); break
