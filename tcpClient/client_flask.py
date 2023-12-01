23ed4eaa3717dfda452537f602153aa192c797cc
hwna00 <layton3534@gmail.com>
2023-12-01 13:53:46 +0900

    <Fix>: 클라이언트 domain_to_address 이벤트 수정
    - emit 하는 값을 사전 정의된 사항에 맞게 수정
    - eval()은 socketIO에서 처리하므로 삭제
    
    ref: #36

--- tcpClient/client_flask.py
+++ tcpClient/client_flask.py
@@ -9,6 +9,7 @@ app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*"}})
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+
 class Client_flask(Client_tcp):
     def connect(self):
         try:
@@ -16,15 +17,17 @@ class Client_flask(Client_tcp):
             print(f"{self.ip}:{self.port} : Connected to Server")
         except TimeoutError as e:
             emit("error", "TimeoutError")
-    
+
     def recv(self):
         try:
             while not self.stop_flag:
                 if self.socket.fileno() == -1:
-                    print(f"{self.ip}:{self.port} : Disconnected by Server"); break
+                    print(f"{self.ip}:{self.port} : Disconnected by Server")
+                    break
                 data = self.socket.recv(65535)
                 if not data:
-                    print(f"{self.ip}:{self.port} : Disconnected from Server"); break
+                    print(f"{self.ip}:{self.port} : Disconnected from Server")
+                    break
                 if data.decode() == "SYN-ACK":
                     emit("handshake_request", {"msg": data.decode()})
                     self.socket.sendall(data)
@@ -41,16 +44,18 @@ class Client_flask(Client_tcp):
             self.socket.close()
             return
 
+
 @socketio.on("welcome")
 def handle_welcome(roomName):
     join_room(roomName)  # TODO: 채팅방 기능이 완성되면 socketIO의 Room 기능은 사용하지 않아도 됨
     emit("welcome", to=roomName)
 
+
 # { "nickname": "홍길동", "domainName": "localhost:3000" }
-@socketio.on('craete_connection')
+@socketio.on("craete_connection")
 def handle_create_connection(data):
     global client_socket
-    data = eval(data)
+
     nickname = data["nickname"]
     ip = gethostbyname(data["domainName"].split(":")[0])
     ip_b = inet_pton(AF_INET, ip)
@@ -59,27 +64,40 @@ def handle_create_connection(data):
     client_socket.start_client_tcp()
     emit("make_connection", {"result": True})
 
-@socketio.on('domain_to_address')
+
+@socketio.on("domain_to_address")
 def handle_domain_to_address(data):
-    data = eval(data)
     domain_list = data["domainName"].split(":")
-    print(gethostbyname(domain_list[0])) # For debug
-    emit("domain_to_address", {gethostbyname(domain_list[0])})
+    print(domain_list)
+
+    ip = gethostbyname(domain_list[0])
+    emit(
+        "domain_to_address",
+        {"ip": ip, "hex": str(inet_pton(AF_INET, ip))},
+    )
+
 
 # { "sender": "홍길동", "roomName": "홍철범", "msg": "반갑수당"}
-@socketio.on('send_msg')
+@socketio.on("send_msg")
 def handle_send_msg(data):
     global client_socket
-    data = eval(data)
-    emit("new_msg", {"targetRoom": data["roomName"], "msg": data["msg"]}, broadcast=True)
+
+    emit(
+        "new_msg", {"targetRoom": data["roomName"], "msg": data["msg"]}, broadcast=True
+    )
     client_socket.send(data)
     msg_b = bytes(data["msg"], "utf-8")
     emit(
-        "msg_to_byte", {"byte": str(msg_b), "orderedByte": "orderedByte"}, to=data["sender"]
+        "msg_to_byte",
+        {"byte": str(msg_b), "orderedByte": "orderedByte"},
+        to=data["sender"],
     )
     emit(
-        "recv_msg", {"sender": data["sender"], "msg": data["msg"], "targetRoom": data["roomName"]}, to=data["roomName"]
+        "recv_msg",
+        {"sender": data["sender"], "msg": data["msg"], "targetRoom": data["roomName"]},
+        to=data["roomName"],
     )
+
 
 @socketio.on("chat_rooms")
 def handle_chat_rooms():
@@ -99,10 +117,12 @@ def handle_chat_rooms():
         ],
     )
 
+
 @socketio.on("destroy_connection")
 def handle_destroy_connection():
     global client_socket
     client_socket.stop_client_tcp()
+
 
 if __name__ == "__main__":
     socketio.run(app, port=8001, debug=True)
